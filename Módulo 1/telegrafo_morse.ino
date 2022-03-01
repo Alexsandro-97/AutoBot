f680d1dc65e701169b604efeb6694669ac2b7d50
Alexsandro Júnior <53157451+Alexsandro-97@users.noreply.github.com>
2022-03-01 11:17:06 -0300

    Aula 1 - Telégrafo Morse

---
+++ Módulo 1/telegrafo_morse.ino
@@ -0,0 +1,127 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // H
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  //--
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+
+  // I
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  //--
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+
+  // D
+  digitalWrite(3,HIGH);
+  delay(1000);
+  digitalWrite(3,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  //--
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+
+  // U
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(3,HIGH);
+  delay(1000);
+  digitalWrite(3,LOW);
+  delay(1000);
+  //--
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+
+  // D
+  digitalWrite(3,HIGH);
+  delay(1000);
+  digitalWrite(3,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  //--
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+
+  // E
+  digitalWrite(4,HIGH);
+  delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  //--
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+  digitalWrite(2,HIGH);
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000);
+}
